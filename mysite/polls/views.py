e2580764a02c398653d74cc2c810ce488f946bce
leobewater <mmbee888@gmail.com>
2024-07-01 19:52:15 -0400

    detail page and throw 404 if id not exist

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
@@ -9,13 +9,23 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     # output = ", ".join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
-    template = loader.get_template("polls/index.html")
+
+    # Long way
+    # template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list, }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+
+    # Short way
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
