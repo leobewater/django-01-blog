52d645955e0f38f693bf289ea5e7a2e8989f8a5d
leobewater <mmbee888@gmail.com>
2024-07-02 13:32:55 -0400

    using class based as view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,22 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
 
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+# Function-base views
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
