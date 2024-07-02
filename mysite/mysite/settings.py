2be9bf17e5d63344e25b01f74ab6cc14c1295178
leobewater <mmbee888@gmail.com>
2024-07-02 19:55:58 -0400

    add progress in apps for search

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+
     # third party
     'taggit',
 
