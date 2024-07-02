a6c74179e22e73d9c674e23a3d34c588f54afc36
leobewater <mmbee888@gmail.com>
2024-07-02 13:42:42 -0400

    add post_share view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,22 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
 
 class PostListView(ListView):
