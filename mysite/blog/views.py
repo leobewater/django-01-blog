d4655be025b5d191867baa5d032e0f3ec97c9c6d
leobewater <mmbee888@gmail.com>
2024-07-02 16:08:22 -0400

    show similar posts based on tag

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
@@ -120,11 +121,22 @@ def post_detail(request, year, month, day, post):
 
     # List of active comments for this post
     comments = post.comments.filter(active=True)
+
     # Form for users to comment
     form = CommentForm()
+
+    # List of similar posts (related posts)
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    # Get all similar tags posts but exclude itself
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    # use Count aggregation function to generate a calculated field "same_tags"
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
 
     return render(request, 'blog/post/detail.html', {
         'post': post,
         'comments': comments,
         'form': form,
+        'similar_posts': similar_posts,
     })
