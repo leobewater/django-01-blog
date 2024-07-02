d46a0c87c7ae2d991ba633cd42a14efeef7dfa80
leobewater <mmbee888@gmail.com>
2024-07-02 16:30:07 -0400

    add show_latest_posts custom template tag

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -11,3 +11,11 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+# Note that the function returns a dictionary of variables instead of a simple value. Inclusion tags have to return a dictionary of values, which is used as the context to render the specified template.
+# {% show_latest_posts 3 %}
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
