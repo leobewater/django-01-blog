52605ed6e6f637aa4b9137815bc78c032ceafe92
leobewater <mmbee888@gmail.com>
2024-07-02 12:10:08 -0400

    add blog urls to project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls', namespace='polls')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
