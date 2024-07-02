56ec57a13376dd5eb5111f1d79d43fa2ec52b917
leobewater <mmbee888@gmail.com>
2024-07-02 11:10:14 -0400

    modify admin view

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    # auto generate slug with title
+    prepopulated_fields = {'slug': ('title',)}
+    # show id instead of name for author
+    raw_id_fields = ['author']
+    # show a row of date selection
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
