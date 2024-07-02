1601a6e6aee6aa556726045a92f83feee3d10280
leobewater <mmbee888@gmail.com>
2024-07-02 05:46:18 -0400

    add inline choices to question in admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+# Add Choice under Question
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+# customize question form in admin
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+
+    # add "Date Information" section title
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
