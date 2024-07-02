af6138ffff9f23c19662c156b25f1dcc508c354a
leobewater <mmbee888@gmail.com>
2024-07-02 10:33:18 -0400

    replace slug with slug field

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     body = models.TextField()
 
     def __str__(self):
