a8d418ddbd856a8a40b1f0c4ac95a0b4e7a6e217
leobewater <mmbee888@gmail.com>
2024-07-02 11:12:20 -0400

    show filter counts

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -12,8 +12,10 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'body']
     # auto generate slug with title
     prepopulated_fields = {'slug': ('title',)}
-    # show id instead of name for author
+    # show id instead of name for author, better when have tons of users
     raw_id_fields = ['author']
     # show a row of date selection
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+    # show facet filters counts
+    show_facets = admin.ShowFacets.ALWAYS
