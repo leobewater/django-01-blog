9d716837a7100593bd5ff90c38d323398412e960
leobewater <mmbee888@gmail.com>
2024-07-02 10:46:15 -0400

    add status text choice

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,10 +3,30 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    # auto_now_add - time will be saved when created
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now update automatically when save
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status, default=Status.DRAFT)
+
+    class Meta:
+        # default sort results in publish DESC order
+        ordering = ['-publish']
+
+        # add index to publish col in -publish Desc order
+        # Index ordering is not supported on MySQL. If you use MySQL for the database, a descending index will be created as a normal index.
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
 
     def __str__(self):
         return self.title
